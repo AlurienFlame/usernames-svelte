d74841b0f6c784298cfa2bc308022e79724d5b5d
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-10-16 06:06:17 -0700

    copy and animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,42 @@
 <script lang="ts">
-    import {NOUNS,ADJECTIVES} from "../data/lists"; 
-let names = ["two", "three", "one"];
+  import { NOUNS, ADJECTIVES } from "../data/lists";
+  import { onMount } from "svelte";
+  let names = [];
 
-const genOneName = () => {
-    let adjective=ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)];
+  const genOneName = () => {
+    let adjective = ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)];
     let noun = NOUNS[Math.floor(Math.random() * NOUNS.length)];
     return adjective + noun;
-}
+  };
 
-const handleGenerate= ()=> {
-    names = new Array(20).fill(0).map(genOneName);
-}
+  const handleGenerate = () => {
+    names = []; //new Array(20).fill(0).map(genOneName);
+  };
+
+  onMount(() => {
+    window.setInterval(() => {
+      if (names.length < 20) {
+        names = [ ...names.slice(0, names.length),genOneName(),];
+      }
+    }, 1000/60);
+  });
+
+  const copy = (str) => {
+    navigator.clipboard.writeText(str);
+  };
 </script>
 
 <h1>Username Generator</h1>
 <p>Generate your very own <a href="https://github.com/AlurienFlame">AlurienFlame</a>-style username!</p>
-<button on:click={handleGenerate}
->Regenerate</button>
-{#each names as names}
-    <p>{names}</p>
-{/each}
+<button on:click={handleGenerate}>Regenerate</button>
+<ul>
+  {#each names as name}
+    <li>
+        <button
+          on:click={() => {
+            copy(name);
+          }}>📋</button>
+      <span>{name}</span>
+    </li>
+  {/each}
+</ul>
