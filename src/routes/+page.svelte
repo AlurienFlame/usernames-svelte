9e0b2e0ee579bdd061aa78e83178d318c22b9a66
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-10-16 05:58:13 -0700

    working
    Co-authored-by: Cyris Kissane <ckissane@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {NOUNS,ADJECTIVES} from "../data/lists"; 
+let names = ["two", "three", "one"];
+
+const genOneName = () => {
+    let adjective=ADJECTIVES[Math.floor(Math.random() * ADJECTIVES.length)];
+    let noun = NOUNS[Math.floor(Math.random() * NOUNS.length)];
+    return adjective + noun;
+}
+
+const handleGenerate= ()=> {
+    names = new Array(20).fill(0).map(genOneName);
+}
+</script>
+
+<h1>Username Generator</h1>
+<p>Generate your very own <a href="https://github.com/AlurienFlame">AlurienFlame</a>-style username!</p>
+<button on:click={handleGenerate}
+>Regenerate</button>
+{#each names as names}
+    <p>{names}</p>
+{/each}
