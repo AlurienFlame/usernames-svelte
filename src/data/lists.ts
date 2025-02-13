39e9767d7e8325d9b6823480bfb5f161dfabf962
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-02-12 16:58:51 -0800

    Add a bunch of words and organize

--- src/data/lists.ts
+++ src/data/lists.ts
@@ -1,79 +1,138 @@
 export const ADJECTIVES = [
+    // Association: Dragon
+    "Draconic",
+    // Association: Fire
     "Flaming",
     "Blazing",
     "Flickering",
     "Burning",
     "Scorching",
-    "Radiant",
-    "Glowing",
     "Searing",
     "Volcanic",
     "Smoldering",
     "Fiery",
     "Magmatic",
+    // Association: Light
+    "Radiant",
+    "Glowing",
     "Visionary",
-    "Perfect",
     "Luminous",
     "Illuminated",
-    "Enlightened",
-    "Astral",
     "Incandescent",
+    // Association: Knowledge
+    "Enlightened",
     "Inspired",
-    "Infernal",
-    "Arcane",
-    "Eldritch",
+    // Association: Universal
     "Celestial",
     "Stellar",
     "Cosmic",
+    "Astral",
+    "Entropic",
+    // Association: Magic
+    "Arcane",
+    "Eldritch",
+    "Mystic",
+    // Association: Godly
+    "Perfect",
     "Divine",
     "Sacred",
+    // Association: Blasphemous
+    "Infernal",
+    "Blasphemous",
+    // Association: Memetic
+    "False",
+    "Cryptic",
+    "Unknown",
+    "Mythic",
+    "Imitation",
+    "Emulated",
+    "Imagined",
+    // Association: Bestial
+    "Wild",
+    // Association: Oceanic
+    "Surging",
+    // Association: Time
     "Relentless",
     "Unceasing",
     "Unrelenting",
-    "Cryptic",
-    "False",
-    "Blasphemous",
+    "Eternal",
+    "Timeless",
+    "Infinite",
+    "Ancient",
+    // Misc
     "Fractal",
+    "Crystal",
 ]
 
 export const NOUNS = [
+    // Association: Dragon
     "Dragon",
     "Drake",
+    "Serpent",
+    "Reptile",
+    "Wyvern",
+    // Association: Fire
     "Flame",
     "Blaze",
     "Conflagration",
-    "Serpent",
-    "Reptile",
     "Flare",
     "Flicker",
-    "Inspiration",
-    "Wyvern",
-    "Ignition",
     "Ember",
     "Inferno",
     "Spark",
     "Pyre",
+    "Ignition",
     "Combustion",
     "Cinder",
     "Phoenix",
     "Firestorm",
     "Ash",
     "Wildfire",
+    // Association: Light
+    "Illumination",
+    // Association: Knowledge
+    "Inspiration",
     "Epiphany",
     "Idea",
     "Aspiration",
-    "Awakening",
-    "Creature",
-    "Apotheosis",
     "Truth",
-    "Beast",
-    "Star",
-    "Waves",
-    "Tide",
     "Clarity",
-    "Blasphemy",
+    // Association: Universal
+    "Star",
+    "Supernova",
+    "Entropy",
+    // Association: Magic
+    "Arcana",
+    // Association: Godly
+    "Apotheosis",
     "Prophet",
+    // Association: Blasphemous
+    "Blasphemy",
+    // Association: Memetic
     "Facsimile",
     "Egregore",
-    "Echo"
+    "Mystery",
+    "Lie",
+    "Myth",
+    "Imitation",
+    "Emulation",
+    "Imagination",
+    // Association: Bestial
+    "Creature",
+    "Beast",
+    // Association: Oceanic
+    "Waves",
+    "Tide",
+    "Ripple",
+    "Current",
+    "Erosion",
+    // Association: Time
+    "Eternity",
+    "Instant",
+    "Moment",
+    "Infinity",
+    // Misc
+    "Fractal",
+    "Echo",
+    "Awakening",
 ]
